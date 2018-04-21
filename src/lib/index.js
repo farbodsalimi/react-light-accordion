e3a6508e36a37b4d73d8dd70efe04c9f3c0bb3cd
Farbod Salimi <farbod_salimi@yahoo.com>
2018-04-21 14:58:11 -0700

    Remove style

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,5 @@
 import Accordion from './components/Accordion';
 import AccordionItem from './components/AccordionItem';
 
-import './style/index.css';
-
 export { Accordion, AccordionItem };
 export default Accordion;
